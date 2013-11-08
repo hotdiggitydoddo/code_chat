7d44deb13ee98b05add51d68431d64408bd8107d
hotdiggitydoddo <hotdiggitydoddo@gmail.com>
2013-11-08 10:42:33 -0800

    Add postgresql support

--- config/adapters.js
+++ config/adapters.js
@@ -16,7 +16,7 @@ module.exports.adapters = {
 
   // If you leave the adapter config unspecified 
   // in a model definition, 'default' will be used.
-  'default': 'disk',
+  'default': 'herokuPostgresql',
 
   // Persistent adapter for DEVELOPMENT ONLY
   // (data is preserved when the server shuts down)
@@ -35,5 +35,13 @@ module.exports.adapters = {
     // so you don't inadvertently push it up if you're using version control
     password: 'YOUR_MYSQL_PASSWORD', 
     database: 'YOUR_MYSQL_DB'
+  },
+
+  herokuPostgresql: {
+
+    module: 'sails-postgresql',
+    host: 'ec2-54-204-27-119.compute-1.amazonaws.com',
+    user: 'xxwawmmwugumgf',
+    password: 'oJHpeYJJQzFEX7vxuL9107xkqv'
   }
 };
